e5a8ac446d8823cc6fa160aa1c9964d963c9bb7b
Dun Mao <moderndelta137@gmail.com>
2026-03-04 18:46:48 +0900

    Fixing dulicated codes in gameStore.ts

--- src/features/simulation/store/gameStore.ts
+++ src/features/simulation/store/gameStore.ts
@@ -31,21 +31,6 @@ type ProgramAdvanceAnimation = {
   name: string
 }
 
-type ProgramAdvanceRule = {
-  id: string
-  name: string
-  sequence: ChipId[]
-  resultChip: BattleChip
-  priority: number
-}
-
-type ProgramAdvanceAnimation = {
-  sourceSlots: number[]
-  targetSlot: number
-  ticksRemaining: number
-  name: string
-}
-
 type PanelPosition = {
   row: number
   col: number
